a3a8607eeb0d96ba41079c769077d646f35b414a
Nguyen Doan Minh Tam <emchuaconguoiyeu1@gmail.com>
2024-02-24 17:52:57 +0700

    video 20 done

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 const express = require('express')
 //import express from 'express'
+const path = require('path')
 const app = express()
 const port = 8080
 
+//template engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/ltnc', (req, res) => {
+  res.send('mtzz00')
+})
+
+app.get('/test', (req, res) => {
+  res.render('sample.ejs')
 })
 
 app.listen(port, () => {
