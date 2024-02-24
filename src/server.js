ecdd9788bee57c85b3131b3ad1e428403457184a
Nguyen Doan Minh Tam <emchuaconguoiyeu1@gmail.com>
2024-02-25 03:33:49 +0700

    3h33p am

--- src/server.js
+++ src/server.js
@@ -2,24 +2,31 @@ const express = require('express')
 //import express from 'express'
 const path = require('path')
 const app = express()
-const port = 8080
+
+require('dotenv').config()
+
+const port = process.env.PORT || 8888
+const hostname = process.env.HOST_NAME
 
 //template engine
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! mtzz00')
 })
 
 app.get('/ltnc', (req, res) => {
   res.send('mtzz00')
 })
 
+//config static file
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/test', (req, res) => {
   res.render('sample.ejs')
 })
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })
